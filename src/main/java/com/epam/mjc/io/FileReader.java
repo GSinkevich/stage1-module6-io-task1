92752551856a91d4f68373bdae3c077921034d5d
DESKTOP-GV962H6\sinke <sinkevich.work@gmail.com>
2023-01-07 16:39:09 +0300

    update catch

--- src/main/java/com/epam/mjc/io/FileReader.java
+++ src/main/java/com/epam/mjc/io/FileReader.java
@@ -7,26 +7,36 @@ public class FileReader {
 
     public Profile getDataFromFile(File file) {
         Profile profile = new Profile();
-        try(FileInputStream fileInputStream = new FileInputStream(file.getPath())) {
+        try (FileInputStream fileInputStream = new FileInputStream(file.getPath())) {
             int ch;
             StringBuilder stringBuilder = new StringBuilder();
             while ((ch = fileInputStream.read()) != -1) {
                 stringBuilder.append((char) ch);
             }
-            String line = stringBuilder.toString();
-            String[] keyValuePair = line.split("[\r\n: ]");
 
-            profile.setName(keyValuePair[2]);
-            profile.setAge(Integer.parseInt(keyValuePair[6]));
-            profile.setEmail(keyValuePair[10]);
-            profile.setPhone(Long.parseLong(keyValuePair[14]));
 
-        } catch (NumberFormatException ex) {
-            throw new NumberFormatException(ex.getMessage());
+           String line = stringBuilder.toString();
+           String[] keyValuePair = line.split("\r\n");
+            for (int i = 0; i < keyValuePair.length; i++) {
+                keyValuePair[i] = keyValuePair[i].replaceAll("\\s", "");
+                String [] mas = keyValuePair[i].split(":",2);
+                keyValuePair[i] = mas[1];
+            }
+
+
+            profile.setName(keyValuePair[0]);
+            int Age = Integer.parseInt(keyValuePair[1]);
+            profile.setAge(Age);
+            profile.setEmail(keyValuePair[2]);
+            long Phone = Long.parseLong(keyValuePair[3]);
+            profile.setPhone(Phone);
+
+        } catch (FileNotFoundException e) {
+            System.out.println(e.getMessage());
         } catch (IOException e) {
-            throw new RuntimeException(e.getMessage());
+            System.out.println(e.getMessage());
         }
-
-        return  profile;
+        return profile;
     }
 }
+
