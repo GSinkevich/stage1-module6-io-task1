97336caf9c08488214fa90d91fc9c42d87657602
DESKTOP-GV962H6\sinke <sinkevich.work@gmail.com>
2023-01-07 14:29:55 +0300

    task done

--- src/main/java/com/epam/mjc/io/FileReader.java
+++ src/main/java/com/epam/mjc/io/FileReader.java
@@ -1,11 +1,30 @@
 package com.epam.mjc.io;
 
-import java.io.File;
+import java.io.*;
 
 
 public class FileReader {
 
     public Profile getDataFromFile(File file) {
-        return new Profile();
+        Profile profile = new Profile();
+        try(FileInputStream fileInputStream = new FileInputStream(file.getPath())) {
+            int ch;
+            StringBuilder stringBuilder = new StringBuilder();
+            while ((ch = fileInputStream.read()) != -1) {
+                stringBuilder.append((char) ch);
+            }
+            String line = stringBuilder.toString();
+            String[] keyValuePair = line.split("[\r\n: ]");
+
+            profile.setName(keyValuePair[2]);
+            profile.setAge(Integer.parseInt(keyValuePair[6]));
+            profile.setEmail(keyValuePair[10]);
+            profile.setPhone(Long.parseLong(keyValuePair[14]));
+
+        } catch (IOException ex) {
+            System.out.println(ex.getMessage());
+        }
+
+        return  profile;
     }
 }
